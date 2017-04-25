f1bfe50bf026add6ff06513261f4cc1c19ea8b7c
Andric Tham <andric@nuisance.co>
2017-04-25 15:39:14 +0800

    Setup React Router skeleton (App.js) and index route

---
+++ client/routes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import App from './components/App';
+import Greetings from './components/Greetings';
+
+export default (
+  <Route path='/' component={App}>
+    <IndexRoute component={Greetings} />
+  </Route>
+)
