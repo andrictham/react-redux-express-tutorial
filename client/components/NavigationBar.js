a383cc4b2223aea24cffcef5a20e85942e6c198b
Andric Tham <andric@nuisance.co>
2017-04-25 15:48:56 +0800

    Changed NavigationBar to use React Router Link

--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default () => (
   <nav className="navbar navbar-default">
     <div className="container-fluid">
       <div className="navbar-header">
-        <a href="#" className="navbar-brand">Red Dice</a>
+        <Link to="/" className="navbar-brand">Red Dice</Link>
       </div>
 
       <div className="collapse navbar-collapse">
         <ul className="nav navbar-nav navbar-right">
-          <li><a href="#">Sign Up</a></li>
+          <li><Link to="/signup">Sign Up</Link></li>
         </ul>
       </div>
     </div>
