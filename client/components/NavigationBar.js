ed941feea416628e83260106f9aed266537b8318
Andric Tham <andric@nuisance.co>
2017-04-25 13:18:07 +0800

    Added Bootstrap navigation bar

---
+++ client/components/NavigationBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default () => (
+  <nav className="navbar navbar-default">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <a href="#" className="navbar-brand">Red Dice</a>
+      </div>
+
+      <div className="collapse navbar-collapse">
+        <ul className="nav navbar-nav navbar-right">
+          <li><a href="#">Sign Up</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+);
