f18d30e875d84c44fe9c87de492d6c4db868bef4
Andric Tham <andric@nuisance.co>
2017-04-28 16:28:51 +0800

    Create users table

--- migrations/20170428081559_users.js
+++ migrations/20170428081559_users.js
@@ -1,8 +1,23 @@
+// Auto-generated from knex cli
+// knex migrate:make users
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('users', function(table) {
+    table.increments(); // an incrementing ID
+    table.string('username').notNullable().unique();
+    table.string('email').notNullable().unique();
+    table.string('timezone').notNullable();
+    table.string('password_digest').notNullable();
+    table.timestamps(); // creates 'created at' and 'updated at' fields
+  });
 };
 
+// down is the reverse of what we are doing in up
+// so, if we are creating a table in up
+// we drop that same table in down
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('users');
 };
+
+// Run migration with the following command in cli:
+// knex migrate:latest
